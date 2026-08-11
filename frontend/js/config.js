83d99968ebca0ebfe68b7452be2596085308f191
killmonger331 <richardmorales_24@hotmail.com>
2026-08-10 21:53:03 -0700

    added login page

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,2 +1,10 @@
 export const API_BASE_URL =
-    "https://mi821y1vh0.execute-api.us-east-1.amazonaws.com/Prod"; 
+  "https://mi821y1vh0.execute-api.us-east-1.amazonaws.com/Prod";
+
+export const COGNITO_REGION = "us-east-1";
+
+export const COGNITO_USER_POOL_ID =
+  "us-east-1_IgIYsDF5i";
+
+export const COGNITO_CLIENT_ID =
+  "7jvbc7l73lgs3p5vc422t54d18";
